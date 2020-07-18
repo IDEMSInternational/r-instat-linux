64277b77a1553f0c43c180b8eff5def3c1262f36
Ian Edwards <isedwards@users.noreply.github.com>
2020-07-18 15:19:38 +0100

    Update setup.sh

--- sh/setup.sh
+++ sh/setup.sh
@@ -4,6 +4,8 @@
 
 ## Setup Wine and dependencies
 
+mkdir /tmp/r-instat-setup; cd /tmp/r-instat-setup
+
 # Wine has significantly better support for 32-bit applications
 sudo dpkg --add-architecture i386
 
@@ -18,11 +20,11 @@ sudo apt update
 # Wine 4.5+ require 32-bit and 64-bit versions of libfaudio0 (which is not available from ubuntu repositories
 wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/amd64/libfaudio0_19.07-0~bionic_amd64.deb
 wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/i386/libfaudio0_19.07-0~bionic_i386.deb
-sudo apt install ./libfaudio0_19.07-0~bionic_amd64.deb
-sudo apt install ./libfaudio0_19.07-0~bionic_i386.deb
+sudo apt install -y ./libfaudio0_19.07-0~bionic_amd64.deb
+sudo apt install -y ./libfaudio0_19.07-0~bionic_i386.deb
 
 # Install the latest wine-stable
-sudo apt install --install-recommends winehq-stable
+sudo apt install -y --install-recommends winehq-stable
 
 
 ## 32-bit .NET manual install tested on Wine 5.0.1
@@ -31,17 +33,28 @@ sudo apt install --install-recommends winehq-stable
 sudo wget 'https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks' -O /usr/local/bin/winetricks
 sudo chmod +x /usr/local/bin/winetricks
 
-# Initialise Wine
-env WINEPREFIX=$HOME/winedotnet WINEARCH=win32 wineboot –init
+# Initialise Wine with 32-bit architecture and supress messages boxes to install Mono and Gecko (for HTML rendering)
+env WINEPREFIX=$HOME/winedotnet WINEARCH=win32 WINEDLLOVERRIDES="mscoree,mshtml=" wineboot –init
 
-# Install .NET 4.0 through winetricks
+# Install of .NET Framework 4.0 through winetricks
+sudo apt-get install -y cabextract  # for corefonts install
+env WINEPREFIX=$HOME/winedotnet WINEARCH=win32 winetricks -q dotnet40 corefonts
 
-sudo apt-get install cabextract  # for corefonts install
-env WINEPREFIX=$HOME/winedotnet WINEARCH=win32 winetricks dotnet40 corefonts
+# Install of .NET Framework 4.6.2 manually
+env WINEPREFIX=$HOME/winedotnet WINEARCH=win32 winetricks win7
+wget 'https://download.microsoft.com/download/E/4/1/E4173890-A24A-4936-9FC9-AF930FE3FA40/NDP461-KB3102436-x86-x64-AllOS-ENU.exe'
+env WINEPREFIX=$HOME/winedotnet WINEARCH=win32 wine ./NDP461-KB3102436-x86-x64-AllOS-ENU.exe /q
 
+# Verify .NET Framework setup
+wget 'https://msdnshared.blob.core.windows.net/media/2018/05/netfx_setupverifier_new.zip'
+unzip netfx_setupverifier_new.zip
+env WINEPREFIX=$HOME/winedotnet WINEARCH=win32 wine ./netfx_setupverifier.exe
+env WINEPREFIX=$HOME/winedotnet WINEARCH=win32 wineboot –e
 
-
-
-
-# Direct link to R-instat Installer 0.6.2 32-bit (not working from Google Drive)
+# Direct link to R-instat Installer 0.6.2 32-bit (not working from Google Drive, see issue GitHub/#1)
+# ** CURRENTLY REQUIRES DOWNLOADING FROM A BROWSER TO ACKNOWLEDGE THAT GOOGLE HASN'T PERFORMED A VIRUS CHECK **
 wget https://drive.google.com/uc?export=download&id=16scomKqnFT_Q2CS8BfZim9nme6jBwk1z
+
+# Install R-Instat
+env WINEPREFIX=$HOME/winedotnet WINEARCH=win32 wineboot –init
+env WINEPREFIX=$HOME/winedotnet WINEARCH=win32 wine ./R-Instat_0.6.2_Installer_32.exe
